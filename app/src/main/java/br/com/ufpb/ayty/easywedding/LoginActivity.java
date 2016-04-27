f15475ef55b35298b399a84ff272af98a150115d
deyvison <deyvison.melo@dce.ufpb.br>
2016-04-27 19:52:03 -0300

    refatorando o codigo

--- app/src/main/java/br/com/ufpb/ayty/easywedding/LoginActivity.java
+++ app/src/main/java/br/com/ufpb/ayty/easywedding/LoginActivity.java
@@ -82,52 +82,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-        /**
-        try {
-            //criação de casamento
-            Casamento c = new Casamento("Jose","Leticia");
-            db.insertCasamento(c);
 
-            //criação dos convidados
-            Convidado convidado1= new Convidado("Joaquim");
-            Convidado convidado2 = new Convidado("Joana");
-            Convidado convidado3 = new Convidado("Josefa");
-            Convidado convidado4 = new Convidado("Rodrigo");
-            convidado1.setCasamento(c);
-            convidado2.setCasamento(c);
-            convidado3.setCasamento(c);
-            convidado4.setCasamento(c);
-            db.insertConvidado(convidado1);
-            db.insertConvidado(convidado2);
-            db.insertConvidado(convidado3);
-            db.insertConvidado(convidado4);
-
-            //criação do usuário
-            Usuario user = new Usuario("jose","jalfdlaf",c);
-            user.setCasamento(c);
-            db.insertUsuario(user);
-
-            //adiciona usuário a uma coleção em casamento
-            c.addUsuario(user);
-
-            //adiciona convidado a uma coleção em casamento
-            c.addConvidado(convidado1);
-            c.addConvidado(convidado2);
-            c.addConvidado(convidado3);
-            c.addConvidado(convidado4);
-
-
-        } catch (Exception e) {
-            Log.e("ayty",e.getMessage());
-        }*/
-        try {
-            List<Usuario> retorno = db.selectUsuario();
-            for(Usuario u : retorno){
-                Log.i("ayty",u.toString());
-            }
-        } catch (SQLException e) {
-            Log.e("ayty", e.getMessage());
-        }
     }
 
     private boolean validarCampos(EditText campoLogin, EditText campoSenha) {
